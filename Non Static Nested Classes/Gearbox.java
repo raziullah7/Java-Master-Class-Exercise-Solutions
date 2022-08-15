d3195207711ca38d17b37d907d341d44dac83167
Razi Ullah <iamraziullah07@gmail.com>
2022-08-15 22:45:40 +0500

    Inner classes Part 1

--- Non Static Nested Classes/Gearbox.java
+++ Non Static Nested Classes/Gearbox.java
@@ -4,6 +4,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -11,16 +12,48 @@ public class Gearbox {
         Gear neutral = new Gear(0, 0.0);
     }
 
+    public void clutchIsIn (boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear (int number, double ratio) {
+        if (number > 0 && number <= maxGears)
+            this.gears.add(new Gear(number, ratio));
+    }
+
+    public void changeGear (int newGear) {
+        if (newGear >= 0 && newGear < this.gears.size() && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        }
+        else {
+            this.currentGear = 0;
+            System.out.println("Grind!");
+        }
+    }
+
+    public double wheelSpeed (int revs) {
+        if (this.clutchIsIn) {
+            System.out.println("Scream!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
     // This class is only applicable in context with the Gearbox class, hence it's
     // an inner class. All public and private methods of Gearbox are accessible to
     // Gear class (non-static inner class)
-    public class Gear {
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
         }
 
         public double driveSpeed (int revs) {
--- Non Static Nested Classes/Main.java
+++ Non Static Nested Classes/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Gearbox mcLaren = new Gearbox(6);
+        // can't access directly because it's not public access
+        /*Gearbox mcLaren = new Gearbox(6);
         // impossible to make an object of Gear class (inner class) without
         // having an instance of the Gearbox class
         Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+        System.out.println(first.driveSpeed(1000));*/
+
+
     }
 }

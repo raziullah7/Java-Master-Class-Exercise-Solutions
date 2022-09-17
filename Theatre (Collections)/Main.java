e7de184a0250e00113a26896a33dc412688b9cc7
Razi Ullah <raziullah913@gmail.com>
2022-09-17 17:59:38 +0500

    shallow copy of a list

--- Theatre (Collections)/Main.java
+++ Theatre (Collections)/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Dana Cummings", 8, 12);
-        // creates a shallow copy
+        // creates a shallow copy, which simply points to the original list items
         List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
         printList(seatCopy);
 
@@ -33,13 +33,16 @@ public class Main {
         System.out.println("Min Seat Number : " + minSeat.getSeatNumber());
         System.out.println("Max Seat Number : " + maxSeat.getSeatNumber());
 
+        sortList(seatCopy);
+        System.out.println("Printing sorted seatCopy");
+        printList(seatCopy);
 
-//        theatre.getSeats();
-//        if ((theatre.reserveSeat("D07"))) {
-//            System.out.println("Reservation available, please pay");
-//        } else {
-//            System.out.println("Seat unavailable");
-//        }
+/*        theatre.getSeats();
+        if ((theatre.reserveSeat("D07"))) {
+            System.out.println("Reservation available, please pay");
+        } else {
+            System.out.println("Seat unavailable");
+        }*/
     }
 
     // method to print a list given as argument
@@ -49,5 +52,28 @@ public class Main {
         }
         System.out.println();
         System.out.println("====================================================");
+    }
+
+    // method to perform bubble sort manually
+    // merge sort requires far more memory than bubble sort
+    // merge sort is faster than bubble sort
+    public static void sortList(List<? extends Theatre.Seat> list){
+    /*
+    The '?' is known as a wildcard. It allows you to broaden the scope of the
+    parameter type, in this case Theatre.Seat. In other words, instead of narrowing
+    the parameter type to just objects of type Theatre.Seat you could include
+    possible subtypes of Theatre.Seat for the List in question
+    */
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    // swapping the contents of i-th and j-th elements
+                    /*Theatre.Seat someSeat = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, someSeat);*/
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
